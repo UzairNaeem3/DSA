eb7b9f34e249c60e27390db57e1a140504427f29
UzairNaeem3 <uzairnaeem303@gmail.com>
2023-07-09 22:39:14 +0500

    Added Reverse Function

--- data_structures/Linked_List/Singly_LL_Interview_Question.py
+++ data_structures/Linked_List/Singly_LL_Interview_Question.py
@@ -20,18 +20,55 @@ class SpecialLinkedList:
         if root is None:
             return "Linked List is empty."
         else:
-            current_node = self.head
+            current_node = root
             output = ''
             while current_node:
                 suffix = ''
-                if current_node.next is None:
-                    suffix = '-->'
+                if current_node.next:
+                    suffix = '-->'  
                 output += str(current_node.value) + suffix
                 current_node = current_node.next
             
             print(output)
             return
+        
+    def insert(self, root, data):
+        new_node = Node(data)
+        
+        if root is None:
+            root = new_node
+        else:
+            new_node.next = root
+            root = new_node
+        
+        self.head = root
+        
+        return root, data
+    
+    def reverse(self, root):
+        if root is None or root.next is None:
+            return root
+        
+        previous = None
+        current = root
+        next = None
+        
+        while current:
+            next = current.next
+            current.next = previous
+            
+            previous = current
+            current = next
+            
+        self.head = previous
 
 if __name__ == "__main__":
     data_list = ["r","e","v","e","r","s","e","d"]
     ll = SpecialLinkedList(data_list)
+    ll.printLL(ll.head)
+    
+    ll.insert(ll.head, 10)
+    ll.printLL(ll.head)
+    
+    ll.reverse(ll.head)
+    ll.printLL(ll.head)
